74c9d097cb4c28fbc787d0b15c674b0ddc48e927
thiagosan593 <thiago_593@hotmail.com>
2023-01-21 21:45:49 -0300

    correções

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+
+
+    const botaoTrailer = document.querySelector('.botao-trailer')
+    const modal = document.querySelector('.modal')
+    const botaoFecharModal = document.querySelector('.fechar-modal')
+    const video = document.getElementById("video")
+    const linkVideo = video.src;
+    
+   
+    function alternarModal(){
+        modal.classList.toggle("aberto")
+
+    }
+
+   
+
+    botaoTrailer.addEventListener("click", () => {
+        alternarModal()
+    // modal.classList.add("aberto")
+     video.setAttribute("src",linkVideo)
+    })
+    
+  botaoFecharModal.addEventListener('click' , () => {
+    alternarModal()
+   //modal.classList.toggle("aberto")
+   video.setAttribute("src","")
+  // modal.remove(fecharvideo)
+  })
